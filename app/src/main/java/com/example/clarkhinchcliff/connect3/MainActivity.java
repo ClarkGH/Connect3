43b5718dba8e563c5c01004405f418114baf96b0
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-15 15:41:59 -0700

    drop functionality.
    -Need to fix some margins

--- app/src/main/java/com/example/clarkhinchcliff/connect3/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/connect3/MainActivity.java
@@ -2,8 +2,19 @@ package com.example.clarkhinchcliff.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view) {
+
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().translationYBy(1000f).setDuration(300);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
