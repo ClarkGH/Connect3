6c7ad205c48be08e225c335a91280780ae7f9b7b
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-15 16:23:42 -0700

    Cannot change players on a spot on the board

--- app/src/main/java/com/example/clarkhinchcliff/connect3/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/connect3/MainActivity.java
@@ -4,24 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //0 = yellow 1 = red
     int activePlayer = 0;
 
+    //2 means unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setTranslationY(-1000f);
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
+        if (gameState[tappedCounter] == 2) {
+            counter.setTranslationY(-1000f);
+            gameState[tappedCounter] = activePlayer;
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
         } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+            Toast.makeText(getApplicationContext(), "This spot is already taken", Toast.LENGTH_LONG).show();
         }
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
 
     }
 
